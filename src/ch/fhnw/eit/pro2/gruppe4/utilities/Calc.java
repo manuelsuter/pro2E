1ee74ab5b6e5a906969f23db942bb6af0dce7896
Anita Rosenberger <anita.rosenberger@bilchen.ch>
2015-05-03 11:26:14 +0200

    added the methods diskConv and schrittIfft

--- src/ch/fhnw/eit/pro2/gruppe4/utilities/Calc.java
+++ src/ch/fhnw/eit/pro2/gruppe4/utilities/Calc.java
@@ -1,4 +1,7 @@
 package ch.fhnw.eit.pro2.gruppe4.utilities;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.ComplexType;
+
 /*
  * Copyright (c) 2015: Anita Rosenberger, Raphael Frey, Benjamin Mueller, Florian Alber, Manuel Suter
  * 
@@ -10,7 +13,7 @@ public class Calc {
 	
 	/**
 	 * Berechnet linspace mittels startValue, endValue und length.
-	 * Gibt ein Array mit den entsprechenden Weten zur�ck.
+	 * Gibt ein Array mit den entsprechenden Weten zur�ck.
 	 * @param startValue
 	 * @param endValue
 	 * @param length
@@ -36,6 +39,57 @@ public class Calc {
 	 * @return
 	 */
 	public static double[] sani(double Tu, double Tg){
+		
+		
+		return null;
+	}
+	
+	/**
+	 *Ausmultiplizieren von Polynomen der Form: (1+x_1)*(1+x_2)*...
+	 * @param x double[] 
+	 * @return ausmultiplikation
+	 */
+	public double[] poly(double[] x) {//wirklich returnvalue ein double[]?
+		double[] ausmulti = new double[x.length];
+		for (int i = 0; i < x.length; i++) {
+			
+		}
+		return ausmulti;
+	}
+	
+	/**
+	 *Berechnet die diskrete Faltung
+	 * @param b = Nenner
+	 * @param a = Zähler
+	 * @return c = die Faltung
+	 */
+	public double[] diskConv(double[] b, double[] a) {
+		double[] c = new double[a.length +b.length -1];
+		
+		for (int n = 0; n < c.length; n++) {
+			for (int i = Math.max(0, n-b.length +1); i <= Math.min(a.length-1, n); i++) {
+				c[i] += a[i]*b[n-i];
+			}
+		}
+		return c;
+	}
+	
+	/**
+	 *Berechnet die Inverse step fast furious transformation 
+	 * @param zah = Zähler 
+	 * @param nen = Nenner
+	 * @param fs = Frequenz
+	 * @param n = Länge Arrey
+	 * @return c = die Faltung
+	 */
+	public double[][] schrittIfft(double[] zah, double[] nen, double[]fs, double n) {
+		double[] T = new double[fs.length];//Periode
+		double[] w = new double[fs.length];//Kreisfrequenz
+		
+		for (int i = 0; i < fs.length; i++) {
+			T[i] = (1/fs[i]);
+			w[i] = logspace(0,fs[i]*Math.PI,n/2);
+		}
 		
 		
 		return null;
