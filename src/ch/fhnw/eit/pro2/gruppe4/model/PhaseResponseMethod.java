0a13f1b471ccc10bebfe3e48025703cbee9621ba
Benjamin <benjamin.mueller@students.fhnw.ch>
2015-06-05 19:13:06 +0200

    Formatierung verschönert

--- src/ch/fhnw/eit/pro2/gruppe4/model/PhaseResponseMethod.java
+++ src/ch/fhnw/eit/pro2/gruppe4/model/PhaseResponseMethod.java
@@ -16,13 +16,19 @@ import ch.fhnw.eit.pro2.gruppe4.utilities.Calc;
 
 public class PhaseResponseMethod extends Controller {
 
-	public static final double OVERSHOOT0 = -1.8099064, OVERSHOOT4_6 = -2.0001473, OVERSHOOT16_3 = -2.2427481,
+	public static final double OVERSHOOT0 = -1.8099064,
+			OVERSHOOT4_6 = -2.0001473, OVERSHOOT16_3 = -2.2427481,
 			OVERSHOOT23_3 = -2.3561945;
 	private double phiU = OVERSHOOT0;
-	// Wenn der Phasenrand-Winkel in Abhängigkeit der Regelstrecken-Ordnung geändert werden soll, kann hier der Ordnung entsprechend der Maximal-Winkel für die Berechnung des logspace gewählt werden.
-	private static final double[] MINIMUMANGLEPI = new double[] { -3.1, -3.1, -3.1, -3.1, -3.1, -3.1, -3.1, -3.1 };
-	private static final double[] MINIMUMANGLEPID = new double[] { -3.1, -3.1, -3.1, -3.1, -3.1, -3.1, -3.1, -3.1 };
-	public static final double PHASEMARGINPI = -1.5707963, PHASEMARGINPID = -2.3561802;
+	// Wenn der Phasenrand-Winkel in Abhängigkeit der Regelstrecken-Ordnung
+	// geändert werden soll, kann hier der Ordnung entsprechend der
+	// Maximal-Winkel für die Berechnung des logspace gewählt werden.
+	private static final double[] MINIMUMANGLEPI = new double[] { -3.1, -3.1,
+			-3.1, -3.1, -3.1, -3.1, -3.1, -3.1 };
+	private static final double[] MINIMUMANGLEPID = new double[] { -3.1, -3.1,
+			-3.1, -3.1, -3.1, -3.1, -3.1, -3.1 };
+	public static final double PHASEMARGINPI = -1.5707963,
+			PHASEMARGINPID = -2.3561802;
 	private double phaseMargin;
 
 	private int omegaControllerIndex;
@@ -44,9 +50,10 @@ public class PhaseResponseMethod extends Controller {
 	 * 
 	 * @param controllerTyp
 	 * @param path
-	 * @throws ControllerException 
+	 * @throws ControllerException
 	 */
-	public void setData(int controllerTyp, Path path) throws ControllerException {
+	public void setData(int controllerTyp, Path path)
+			throws ControllerException {
 		this.controllerTyp = controllerTyp;
 		this.path = path;
 		Tp = 0.0;
@@ -61,9 +68,10 @@ public class PhaseResponseMethod extends Controller {
 	 * @param controllerTyp
 	 * @param path
 	 * @param Tp
-	 * @throws ControllerException 
+	 * @throws ControllerException
 	 */
-	public void setData(int controllerTyp, Path path, double Tp) throws ControllerException {
+	public void setData(int controllerTyp, Path path, double Tp)
+			throws ControllerException {
 		this.controllerTyp = controllerTyp;
 		this.path = path;
 		this.Tp = Tp;
@@ -78,9 +86,10 @@ public class PhaseResponseMethod extends Controller {
 	 * @param controllerTyp
 	 * @param path
 	 * @param Tp
-	 * @throws ControllerException 
+	 * @throws ControllerException
 	 */
-	public void setData(int controllerTyp, Path path, double Tp, double overShoot, double phaseMargin) throws ControllerException {
+	public void setData(int controllerTyp, Path path, double Tp,
+			double overShoot, double phaseMargin) throws ControllerException {
 		this.controllerTyp = controllerTyp;
 		this.path = path;
 		this.Tp = Tp;
@@ -117,6 +126,7 @@ public class PhaseResponseMethod extends Controller {
 
 	/**
 	 * Setzt Krk.
+	 * 
 	 * @param Krk
 	 */
 	public void setKrk(double Krk) {
@@ -137,8 +147,8 @@ public class PhaseResponseMethod extends Controller {
 	}
 
 	/**
-	 * Löst createOmegaAxis() aus und berechnet Hs der Regelstrecke.
-	 * Löst calculateTnk() aus. 
+	 * Löst createOmegaAxis() aus und berechnet Hs der Regelstrecke. Löst
+	 * calculateTnk() aus.
 	 */
 	protected void calculate() throws ControllerException {
 		// UTF Strecke aus Strecke holen
@@ -156,7 +166,8 @@ public class PhaseResponseMethod extends Controller {
 		}
 		for (int i = 0; i < Hs.length; i++) {
 			for (int n = 0; n < Ts.length; n++) {
-				Hs[i] = Hs[i].multiply(new Complex(1).divide(new Complex(1, Ts[n] * omega[i])));
+				Hs[i] = Hs[i].multiply(new Complex(1).divide(new Complex(1,
+						Ts[n] * omega[i])));
 			}
 		}
 		phiS = Calc.ComplexAngleUnwraped(Hs);
@@ -166,8 +177,9 @@ public class PhaseResponseMethod extends Controller {
 	}
 
 	/**
-	 * Berechnet Tnk und Tvk anhand der Phasengang-Methode.
-	 * Löst calculateKrk() aus.
+	 * Berechnet Tnk und Tvk anhand der Phasengang-Methode. Löst calculateKrk()
+	 * aus.
+	 * 
 	 * @throws ControllerException
 	 */
 	private void calculateTnkTvk() throws ControllerException {
@@ -206,7 +218,8 @@ public class PhaseResponseMethod extends Controller {
 				Tnk = 1 / (omegaController * beta);
 
 				for (int i = 0; i < pointNumber; i++) {
-					Hr[i] = new Complex(1, omega[i] * Tnk).multiply(new Complex(1, omega[i] * Tvk)).divide(
+					Hr[i] = new Complex(1, omega[i] * Tnk).multiply(
+							new Complex(1, omega[i] * Tvk)).divide(
 							new Complex(0, omega[i] * Tnk));
 					Ho[i] = Hs[i].multiply(Hr[i]);
 				}
@@ -237,8 +250,8 @@ public class PhaseResponseMethod extends Controller {
 	}
 
 	/**
-	 * Berechnet Krk anhand der Phasengang-Methode.
-	 * Löst calculateControllerConf() und setUTF() aus.
+	 * Berechnet Krk anhand der Phasengang-Methode. Löst
+	 * calculateControllerConf() und setUTF() aus.
 	 */
 	private void calculateKrk() {
 		// Krk berechnen
@@ -271,7 +284,8 @@ public class PhaseResponseMethod extends Controller {
 	 * Berechnet die reglerkonformen Attribute.
 	 */
 	private void calculateControllerConf() {
-		double[] controllerConf = Calc.controllerConform(Krk, Tnk, Tvk, Tp, controllerTyp);
+		double[] controllerConf = Calc.controllerConform(Krk, Tnk, Tvk, Tp,
+				controllerTyp);
 		Kr = controllerConf[0];
 		Tn = controllerConf[1];
 		Tv = controllerConf[2];
@@ -279,6 +293,7 @@ public class PhaseResponseMethod extends Controller {
 
 	/**
 	 * Setzt den Phasenrand entsprechend des Reglertyps.
+	 * 
 	 * @throws ControllerException
 	 */
 	private void calculatePhaseMargin() throws ControllerException {
@@ -311,7 +326,9 @@ public class PhaseResponseMethod extends Controller {
 	}
 
 	/**
-	 * Erstellt die Omega-Achse in Abhängigkeit vom Phasengang und des benötigten Winkelbereichs
+	 * Erstellt die Omega-Achse in Abhängigkeit vom Phasengang und des
+	 * benötigten Winkelbereichs
+	 * 
 	 * @param Ts
 	 * @return
 	 */
@@ -337,7 +354,8 @@ public class PhaseResponseMethod extends Controller {
 			phi = 0.0;
 			borderMin--;
 			for (int n = 0; n < Ts.length; n++) {
-				phi -= new Complex(1, Math.pow(10, borderMin) * Ts[n]).getArgument();
+				phi -= new Complex(1, Math.pow(10, borderMin) * Ts[n])
+						.getArgument();
 			}
 		}
 
@@ -346,11 +364,13 @@ public class PhaseResponseMethod extends Controller {
 			phi = 0.0;
 			borderMax++;
 			for (int n = 0; n < Ts.length; n++) {
-				phi -= new Complex(1, Math.pow(10, borderMax) * Ts[n]).getArgument();
+				phi -= new Complex(1, Math.pow(10, borderMax) * Ts[n])
+						.getArgument();
 			}
 		}
 
-		return Calc.logspace(borderMin, borderMax, (borderMax - borderMin) * 5000);
+		return Calc.logspace(borderMin, borderMax,
+				(borderMax - borderMin) * 5000);
 
 	}
 }
